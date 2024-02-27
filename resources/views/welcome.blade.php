3fa7b72c55128493afb11d8628561032a60694e4
Diego Giordano <129299839+GiordanoDiego@users.noreply.github.com>
2024-02-27 09:43:56 +0100

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('page-title', 'Home')
+
+{{-- 
+    questa pagina è collegata a web.php che a sua volta avrà il contenuto contenente iin lauouts, layouts sarà composto da 3 componenti che sono header main e footer.blade.php
+--}}
